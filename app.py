359e532c1605070807c107b8a610a23a1aeaf880
Edwinkorir38 <ekorir99@gmail.com>
2026-04-18 03:48:20 +0300

    Fix clApp initialization for Gunicorn

--- app.py
+++ app.py
@@ -3,35 +3,37 @@ import os
 from flask_cors import CORS, cross_origin 
 from cnnClassifier.utils.common import decodeImage 
 from cnnClassifier.pipeline.predict import PredictionPipeline 
-
+ 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
-
+ 
 app = Flask(__name__)
 CORS(app)
-
-
+ 
+ 
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(filename=self.filename)
-
-
+ 
+ 
+# Initialize outside __main__ so Gunicorn can access it
+clApp = ClientApp()
+ 
+ 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
-
-
-
-@app.route("/train", methods=['GET','POST'])
+ 
+ 
+@app.route("/train", methods=['GET', 'POST'])
 @cross_origin()
 def trainRoute():
     os.system("python main.py")
     return "Training done successfully!"
-
-
-    
+ 
+ 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
@@ -39,9 +41,8 @@ def predictRoute():
     decodeImage(image, clApp.filename)
     result = clApp.classifier.predict()
     return jsonify(result)
-
-
-
+ 
+ 
 if __name__ == "__main__":
-    clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080, debug= True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+ 
